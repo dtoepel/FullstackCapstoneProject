d04bd738c6337c8549e6d549dcd3ff08fc67ad0a
Daniel Töpel <toepel@gmx.it>
2025-08-29 18:03:28 +0200

    Added frontend table for archived elections, make election action buttons depend on status

--- frontend/src/ElectionTable.tsx
+++ frontend/src/ElectionTable.tsx
@@ -7,25 +7,28 @@ export type ElectionTableProps = {
     onCreateElection:()=>void;
     onEditElection:(election:Election)=>void;
     onGetResult:(election:Election)=>void;
+    isArchive:boolean;
 }
 
 export default function ElectionTable(props:Readonly<ElectionTableProps>) {
     return(
         <>
-            <button onClick={() => props.onCreateElection()}>Create</button>
+        {props.isArchive?"":<button onClick={() => props.onCreateElection()}>Create</button>}
             <table border={1}>
                 <thead>
                 <tr>
                     <th>ID</th>
                     <th>Name</th>
-                    <th>Status</th>
                     <th>Info</th>
                     <th>Candidates</th>
+                    <th>Seats</th>
                     <th>Actions</th>
                 </tr>
                 </thead>
                 <tbody>
-                {props.elections.map((election) => {
+                {props.elections
+                    .filter(election => (election.electionState === "ARCHIVED") === props.isArchive)
+                    .map((election) => {
                     let candidates:Candidate[] = [];
                     election.candidateIDs.forEach((id) => {
                         candidates = candidates.concat(props.candidates.filter(
@@ -36,16 +39,35 @@ export default function ElectionTable(props:Readonly<ElectionTableProps>) {
                         <tr key={election.id}>
                             <td>{election.id}</td>
                             <td>{election.name}</td>
-                            <td>{election.electionState}</td>
                             <td>{election.description}</td>
                             <td><div style={{display:"flex", flexDirection:"row", flexWrap:"wrap"}}>
                                 {candidates.map(candidate => {return(
                                     <CandidateBox candidate={candidate}/>
                                 )})}
                             </div></td>
+                            <td>{election.seats}</td>
                             <td>
-                                <button onClick={() => props.onEditElection(election)}>Edit</button>
-                                <button onClick={() => props.onGetResult(election)}>Result</button>
+                                {election.electionState === "OPEN"?
+                                    <>
+                                        <button onClick={() => props.onEditElection(election)}>Edit</button>
+                                        <button><s>Open Voting</s></button>
+                                    </>
+                                :election.electionState === "VOTING"?
+                                    <>
+                                        <button><s>Close Voting</s></button>
+                                        <button><s>Vote</s></button>
+                                    </>
+                                :election.electionState === "CLOSED"?
+                                    <>
+                                        <button onClick={() => props.onGetResult(election)}>Result</button>
+                                        <button><s>Archive</s></button>
+                                    </>
+                                :
+                                    <>
+                                        <button onClick={() => props.onGetResult(election)}>Result</button>
+                                    </>
+                                }
+                                <button><s>Delete</s></button>
                             </td>
                         </tr>
                     )})}
